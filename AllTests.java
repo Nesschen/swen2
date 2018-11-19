f479bd6b2c0ac6f7e63551a9a4925b4c9875d6f4
Nesschen <nessi1612@gmx.de>
2018-11-19 20:32:53 +0100

    first commit

---
+++ AllTests.java
@@ -0,0 +1,11 @@
+package SpaghettiCode;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ParkingMeterTest.class })
+public class AllTests {
+
+}
